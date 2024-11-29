b5772597d4dfaa6c1261c31f2e5d4cf4c08d29cb
Lenur Serveriiev <lenur.serveriev@gmail.com>
2024-11-29 14:29:11 +0100

    added spring boot

---
+++ wordbuch/src/main/java/de/wordbuch/WordbuchApplication.java
@@ -0,0 +1,13 @@
+package de.wordbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordbuchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordbuchApplication.class, args);
+    }
+
+}
